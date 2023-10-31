a36e9ec1e1ba9af3bf40b95c68f8561bcd49ca75
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-10-31 20:22:39 +0100

    the super keyword

---
+++ src/SuperKeyword.java
@@ -0,0 +1,18 @@
+/**
+ *  1. super can be used to refer immediate parent class      instancevariable.
+    2. super can be used to invoke immediate parent classmethod.
+    3. super() can be used to invoke immediate parent classconstructor.
+ */
+
+class Animal {
+    String color = "white";
+}
+
+class Dog extends Animal {
+    String color = "black";
+
+    void printColor() {
+        System.out.println(color); // prints color of Dog class
+        System.out.println(super.color); // prints color of Animal class
+    }
+}
